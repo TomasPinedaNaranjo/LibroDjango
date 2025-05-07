35091231d3705a98b56123aae10ccd7bf9ce0687
GitHub Action <action@github.com>
2025-05-07 23:08:21 +0000

    Aplicar formato automático con black e isort

--- home/views.py
+++ home/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    template_data = {}   
+    template_data = {}
 
+    template_data["title"] = "Movies Store"
+    return render(request, "home/index.html", {"template_data": template_data})
 
-    template_data['title'] =   'Movies Store'
-    return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
     template_data = {}
-    
-    template_data['title'] = 'About'
+
+    template_data["title"] = "About"
     unused_variable = 42  # Variable sin usar
 
-    return render(request,  'home/about.html', {'template_data': template_data})
+    return render(request, "home/about.html", {"template_data": template_data})
