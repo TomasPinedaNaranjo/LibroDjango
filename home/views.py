bca459602f9d65bc1115df3095c941c585a538a5
TomasPinedaNaranjo <104210520+TomasPinedaNaranjo@users.noreply.github.com>
2025-05-07 18:00:14 -0500

    fix action 3

--- home/views.py
+++ home/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    template_data = {}
+    template_data = {}   
+
+    
     template_data['title'] =   'Movies Store'
     return render(request, 'home/index.html', {'template_data': template_data})
 
@@ -12,4 +14,4 @@ def about(request):
     template_data['title'] = 'About'
     unused_variable = 42  # Variable sin usar
 
-    return render(request, 'home/about.html', {'template_data': template_data})
+    return render(request,  'home/about.html', {'template_data': template_data})
